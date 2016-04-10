e781ee15aae3f9b060cfd481232fb827b992f1f3
snollygolly <snollygolly@gmail.com>
2016-04-09 22:53:26 -0500

    added better collection and playback, plus dynamic injection
    and red!

--- index.js
+++ index.js
@@ -1,28 +1,71 @@
 "use strict";
 
-const tessel = require('tessel');
-const infraredlib = require('ir-attx4');
-const infrared = infraredlib.use(tessel.port['A']);
+const http = require("http");
+const IP = require('os').networkInterfaces().wlan0[0].address;
+const PORT = 8000;
 
-const codes = {
-	on: require("./codes/on.json"),
-	off: require("./codes/off.json")
-};
+const co = require("co");
+const Promise = require("bluebird");
+const fs = require("fs");
 
-// When we're connected
-infrared.on('ready', function() {
-	console.log("Connected to IR!");
-	infrared.sendRawSignal(38, new Buffer(codes.on), function(err) {
-		if (err) {
-			console.log("Unable to send signal: ", err);
-		} else {
-			console.log("Signal sent!");
+const tessel = require("tessel");
+const infraredlib = require("ir-attx4");
+const infrared = Promise.promisifyAll(infraredlib.use(tessel.port["A"]));
+
+let codes = {};
+setCodes();
+
+function setCodes() {
+	const files = fs.readdirSync(`${__dirname}/codes`);
+	for (const file of files) {
+		const fileName = file.split(".").shift();
+		codes[fileName] = require(`./codes/${file}`);
+	}
+	console.log(`Set ${files.length} codes`);
+}
+
+const server = http.createServer(function (request, response) {
+	const codeType = request.url.split("/").pop();
+	co(function *(){
+		// yield any promise
+		console.log(codes[codeType]);
+		const result = yield infrared.sendRawSignalAsync(38, new Buffer(codes[codeType]));
+		if (result) {
+			// there shouldn't be a result, that means there was an error
+			throw result;
 		}
+		response.writeHead(200, {"Content-Type": "text/plain"});
+		response.end("OK\n");
+	}).catch((err) => {
+		console.error(err.stack);
+		response.writeHead(500, {"Content-Type": "text/plain"});
+		response.end("Error\n");
 	});
 });
 
+server.listen(PORT);
+
+console.log(`Server running at http://${IP}:${PORT}/`);
+
+// light listeners
+
+// When we"re connected
+infrared.on("ready", function() {
+	console.log("Connected to IR!");
+});
+
 // If we get data, print it out
-infrared.on('data', function(data) {
-	console.log("Received RX Data:");
-	console.log(data.toJSON());
+infrared.on("data", function(data) {
+	const now = Date.now();
+	const code = data.toJSON().data;
+	console.log(`Received RX Data: ${data.toString("hex").length}`);
+	console.log(code);
+	if (code.length < 10) {
+		// not sure what this is, I don't want it though
+		console.log("Discarding...");
+		return;
+	}
+	fs.writeFileSync(`${__dirname}/codes/${now}.json`, JSON.stringify(code));
+	console.log(`Wrote file as ${now}.json`);
+	setCodes();
 });
